a3144ad315e4970e48dba100a2f6c8d22f235ffc
Steve J <steveja007@gmail.com>
2022-11-16 11:34:08 -0600

    add option to encrypt

--- src/network.tsx
+++ src/network.tsx
@@ -1,5 +1,10 @@
-// const server = (window.location.hostname === "localhost") ? 'http://localhost:8080' : 'https://tdnode.onrender.com' //'https://sj-td.herokuapp.com'  //https://resultify.live'
-const server = 'https://tdnode.onrender.com'
+import { log } from './util'
+
+import Cryptr from 'cryptr';
+const cryptr = new Cryptr(process.env.KEY42 || "foo");
+const debuggingServerLocally = false
+const server = (debuggingServerLocally && window.location.hostname === "localhost") ? 'http://localhost:8080' : 'https://tdnode.onrender.com'
+ //'https://sj-td.herokuapp.com'  //https://resultify.live'
 
 /**
 	 * Performs http get request from our node.js server
@@ -27,26 +32,32 @@ export async function get(route: string) {
  * @param {string} route route to use.
  * @return [response in JSON(null if error), error object]
  */
-export async function post(data: Record<string, unknown>, route = 'feedback'): Promise<[boolean, string]> {
+export async function post(data: Record<string, unknown>, route = 'feedback', encryptBody = false): Promise<[boolean, string]> {
+	let body = JSON.stringify(data)
+	let contentType = 'application/json'
 
+	if (encryptBody) {
+		body = cryptr.encrypt(body)
+		contentType = 'text/plain'
+	}
 	try {
 		const response = await fetch(`${server}/${route}`, {
 			method: 'POST',
-			body: JSON.stringify(data),
+			body,
 			headers: {
 				'Accept': 'application/json',
-				'Content-Type': 'application/json',
+				'Content-Type': contentType,
 			},
 
 		});
 		if (response.ok)
 			return [true, ""];
-	let text = await response.text()
-	return [false, `error: status ${response.status} ${response.statusText} ${text}`]
-}
-		
-		catch (error:any) {
-	console.error(`post: error occurred with fetch ${error}`);
-	return [false, error.toString()]
-}
+		let text = await response.text()
+		return [false, `error: status ${response.status} ${response.statusText} ${text}`]
 	}
+
+	catch (error: any) {
+		console.error(`post: error occurred with fetch ${error}`);
+		return [false, error.toString()]
+	}
+}
