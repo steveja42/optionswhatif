780b57ce09bc1b5cf895647220b0a4294e38f0c5
Steve J <steveja007@gmail.com>
2024-07-05 18:09:37 -0500

    add route to force new auth

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@
 import ReactGA from 'react-ga'; // https://github.com/react-ga/react-ga
 import React, { useEffect } from 'react';
 import { trackPage } from './util'
+import { getNewTokens } from './schwab'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
@@ -29,7 +30,7 @@ if (googleAnalyticsTrackingID) {
   ReactGA.initialize(googleAnalyticsTrackingID);   // Add your tracking ID created from https://analytics.google.com/analytics/web/#home/
 }
 
-function setPageview(url:string) {
+function setPageview(url: string) {
   log(url)
   trackPage(url)
   ReactGA.pageview(url)
@@ -56,7 +57,7 @@ function App() {
           <img src="../favicon.ico" width="30" height="30" className="d-inline-block align-top" alt="" />
           {' '}
           OptionsWhatIf
-          </Navbar.Brand>
+        </Navbar.Brand>
         <Nav >
           <Nav.Link as={NavLink} to="/about">About</Nav.Link>
           <Nav.Link as={NavLink} to="/aboutaddon">Sheets Add-On</Nav.Link>
@@ -79,6 +80,9 @@ function App() {
           </Row>
         </Container>
       </Route>
+      <Route path="/newauth">
+        <NewAuth />
+      </Route>
       <Route path="/">
         <OptionsWhatIf />
       </Route>
@@ -87,6 +91,24 @@ function App() {
 
 }
 
+const handleSubmit = async (event: React.SyntheticEvent) => {
+  console.log(`"handling form"  `)
+  event.preventDefault();
+  getNewTokens()
+}
+
+
+function NewAuth() {
+  return (
+    <form onSubmit={handleSubmit}  >
+      <button type="submit" value="Submit" >
+        Submit
+        
+      </button>
+      <p />
+    </form>
+  )
+}
 
 function AboutSheetsAddon() {
   return (
@@ -97,12 +119,12 @@ function AboutSheetsAddon() {
       <p>It can help guide your investment decisions by creating data sheets that show potential stock option profit and ROI at various price points.</p>
       <p>
         OptionsWhatIf is available as a Google Sheets add-on and can be obtained
-    <a href="https://gsuite.google.com/marketplace/app/optionswhatif/381044359711?pann=cwsdp&hl=en" rel="noopener noreferrer" target="_blank"> here</a>.
-     </p>
+        <a href="https://gsuite.google.com/marketplace/app/optionswhatif/381044359711?pann=cwsdp&hl=en" rel="noopener noreferrer" target="_blank"> here</a>.
+      </p>
       <p>After installing the add-on in Sheets, select the Sheets menu option Add-ons.OptionsWhatIf.Start.</p>
       <p>Enter a stock symbol (US markets) and choose the desired option expiration dates and a new sheet will be inserted showing price data and potential
         profit and ROI at six different price points- price changes of 10%, 33%,50%, 66%, 90%, and 100%.
-  Price data on a sheet can be updated by selecting the "Update prices" button on the sidebar. The mid point of the bid/ask is used as the option price. Profit is based on intrinsic value.</p>
+        Price data on a sheet can be updated by selecting the "Update prices" button on the sidebar. The mid point of the bid/ask is used as the option price. Profit is based on intrinsic value.</p>
       <Image src={sheetsAddOnScreenshot} alt='screenshot of Google Sheet' className="graphic" />
     </div>)
 }
@@ -116,12 +138,12 @@ function AboutWebApp() {
       <p>It can help guide your investment decisions by showing the potential stock option profit and ROI at various price points.</p>
       <p>
         The OptionsWhatIf Web App can be found
-    <Link to="/"> here</Link>.
-     </p>
+        <Link to="/"> here</Link>.
+      </p>
 
       <p>Enter a stock symbol (US markets) and choose the desired option expiration dates and the page will show price data and potential
         profit and ROI at seven different price points- price changes of 0% 10%, 33%,50%, 66%, 90%, and 100%. You can edit the price points to see customized results.
-  Price data can be updated by selecting the "Refresh" button. Checking "Auto Refresh" will upate the prices every second. The mid point of the bid/ask is used as the option price. Profit is based on intrinsic value.</p>
+        Price data can be updated by selecting the "Refresh" button. Checking "Auto Refresh" will upate the prices every second. The mid point of the bid/ask is used as the option price. Profit is based on intrinsic value.</p>
       <Image src={webAppScreenshot} alt='screenshot of Web App' className="graphic" />
     </div>)
 }
