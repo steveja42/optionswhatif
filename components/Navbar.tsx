0e878aca72e191ac3d45c3a098e4ed73d96e7046
Steve J <steveja007@gmail.com>
2026-03-27 08:40:49 -0700

    Control+click on logo goes to admin page.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,24 @@
 'use client'
 
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import Image from 'next/image'
 
 export function Navbar() {
   const pathname = usePathname()
+  const router = useRouter()
+
+  function handleLogoClick(e: React.MouseEvent) {
+    if (e.ctrlKey) {
+      e.preventDefault()
+      router.push('/owiadmin')
+    }
+  }
 
   return (
     <header className="App-header">
       <nav className="navbar navbar-expand-sm navbar-light bg-light" style={{padding: 0, minHeight: '56px'}}>
-        <Link href="/" className="navbar-brand d-flex align-items-center" style={{ margin: 0, height: '56px'}}>
+        <Link href="/" className="navbar-brand d-flex align-items-center" style={{ margin: 0, height: '56px'}} onClick={handleLogoClick}>
           <Image src="/logo100.png" width={0} height={0} sizes="56px" style={{width: 'auto', height: '100%', marginRight: '8px'}} className="d-inline-block" alt="" />
           OptionsWhatIf
         </Link>
